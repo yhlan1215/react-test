d000d3ea24f9ee985c620eb923dd90d8c66b9137
yhlan1215 <1227353662@qq.com>
2022-07-21 16:26:32 +0800

    1

--- src/screens/project-list/AuthorList.jsx
+++ src/screens/project-list/AuthorList.jsx
@@ -1,24 +1,31 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 
-export const AuthorList = () => {
-    const [authors, setAuthors] = useState([])
+export const AuthorList = ({id}) => {
+    const [author, setAuthor] = useState([])
 
     useEffect(() => {
-        const getAuthors = async () => {
+        if(id){
+        const getAuthor = async () => {
         const {data} = await axios({
             method:'get',
-            url:'http://localhost:8080/authors',
+            url:`http://localhost:8080/authors${author.id}`,
           })
-          setAuthors(data)
+          setAuthor(data)
         }
-        getAuthors();
-    }, [])
+        getAuthor();
+    }
+    }, [id])
 
 
-    return <ul>
-        {
-            authors.map((author) => <li key={author.id}>{author.name}</li>)
-        }
-    </ul>
+    return( 
+        <div style={{marginTop:"10px"}}>
+            <div>作者</div>
+    <select style={{width:"150px"}} value={author.id}>
+        <option value="62d283e95b4af466755e9ecb">马伯庸</option>
+        <option value="62d284b85b4af466755e9ede">南派三叔</option>
+        <option value="62d285325b4af466755e9eed">莫言</option>
+    </select>
+    </div>    
+    )
 }
--- src/screens/project-list/BookDetail.jsx
+++ src/screens/project-list/BookDetail.jsx
@@ -3,6 +3,17 @@ import { useEffect,useState } from "react";
 export const BookDetail = ({id, onBookSaved}) => {
     const[book,setBook]=useState(undefined)
     const[originalBook,setOriginalBook]=useState(undefined)
+    const[authors,setAuthors]=useState([])
+    useEffect(()=>{
+        const getAuthors = async() => {
+            const{data}=
+            await axios({
+                url:"http://localhost:8080/authors"
+            })
+            setAuthors(data)
+        }
+        getAuthors()
+    },[])
 
     useEffect(()=>{
         if(id){
@@ -72,27 +83,29 @@ export const BookDetail = ({id, onBookSaved}) => {
     if (!book) return null;
     return (
         <div><div>书名：</div>
-            <input type="text" value={book.name} onChange={(event) => {
+            <input style={{width:"150px"}} type="text" value={book.name} onChange={(event) => {
                     const clonedBook = JSON.parse(JSON.stringify(book))
                     clonedBook.name = event.currentTarget.value
                     setBook(clonedBook)
                 }}/>
-            <div style={{marginTop:"10px"}}>{"id:"+book.id}</div>
+            <div style={{marginTop:"20px"}}>{"id:"+book.id}</div>
             <div>
-                <div style={{marginTop:"10px"}}>作者：</div>
-                <input type="string" value={book.author.id} onChange={(event) => {
+                <div style={{margintop:"20px"}}>作者</div>
+                <select style={{width:"150px"}} value={book.author.id}  onChange={(event)=>{ 
                     const clonedBook = JSON.parse(JSON.stringify(book))
-                    clonedBook.author.id = event.currentTarget.value
-                    setBook(clonedBook)
-                }}/>
-                <div style={{marginTop:"10px"}}>价格: </div>
-                <input type="number" value={book.price} onChange={(event) => {
+                    clonedBook.author.id = event.currentTarget.selectedOptions[0].value
+                    setBook(clonedBook) 
+                }}>
+                {authors.map((author) => <option value={author.id}>{author.name}</option>)}
+                </select>
+                <div style={{marginTop:"20px"}}>价格: </div>
+                <input style={{width:"150px"}} type="number" value={book.price} onChange={(event) => {
                     const clonedBook = JSON.parse(JSON.stringify(book))
                     clonedBook.price = parseFloat(event.currentTarget.value)
                     setBook(clonedBook)
                 }}/>
-                <div style={{marginTop:"10px"}}>语言：</div>
-                <select value={book.language}
+                <div style={{marginTop:"20px"}}>语言：</div>
+                <select value={book.language} style={{width:"150px"}}
                 onChange={(event)=>{ 
                     const clonedBook = JSON.parse(JSON.stringify(book))
                     clonedBook.language = event.currentTarget.selectedOptions[0].value
@@ -101,20 +114,20 @@ export const BookDetail = ({id, onBookSaved}) => {
                     <option value="English">英语</option>
                     <option value="Chinese">中文</option>
                 </select>
-                <div style={{marginTop:"10px"}}>新旧: </div>
+                <div style={{marginTop:"20px"}}>新旧: </div>
                 <input type="radio" name="isOld" checked={!book.isOld} onChange={(event) => {
                     const clonedBook = JSON.parse(JSON.stringify(book))
                     clonedBook.isOld = false
                     setBook(clonedBook)
                 }}/>新
-                <input type="radio" name="isOld" checked={book.isOld} style={{marginLeft: "10px"}} onChange={(event) => {
+                <input type="radio" name="isOld" checked={book.isOld} style={{marginLeft: "80px"}} onChange={(event) => {
                     const clonedBook = JSON.parse(JSON.stringify(book))
                     clonedBook.isOld = true
                     setBook(clonedBook)
                 }}/>旧
-                <div style={{marginTop:"10px"}}>类别：</div>
+                <div style={{marginTop:"20px"}}>类别：</div>
 
-                <select value={book.category}  onChange={(event) => {
+                <select style={{width:"150px"}} value={book.category}  onChange={(event) => {
                     const clonedBook = JSON.parse(JSON.stringify(book))
                     clonedBook.category = event.currentTarget.selectedOptions[0].value
                     setBook(clonedBook)
@@ -128,8 +141,8 @@ export const BookDetail = ({id, onBookSaved}) => {
                     <option value= "map geography">map geography</option>
                 </select>
             </div>
-            <button style={{marginTop:"10px"}} onClick={onSave}>保存</button>
-            <button style={{marginTop:"10px", marginLeft: "30px"}} onClick={onCancel}>取消</button>
+            <button style={{marginTop:"20px"}} onClick={onSave}>保存</button>
+            <button style={{marginTop:"20px", marginLeft: "30px"}} onClick={onCancel}>取消</button>
         </div>
     )
 }
